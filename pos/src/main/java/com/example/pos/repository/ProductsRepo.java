cb2ad4216427842d989fdc2e5b72cf28983a1510
kavindu ishara <161452109+kavinduishara@users.noreply.github.com>
2025-06-29 23:15:20 +0530

    Revert "error"
    This reverts commit 107bb2870185e392cf022d19a0a5dfedcb222b4f.

--- pos/src/main/java/com/example/pos/repository/ProductsRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.pos.repository;
-
-import com.example.pos.entities.Products;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductsRepo extends JpaRepository<Products,Long> {
-}
